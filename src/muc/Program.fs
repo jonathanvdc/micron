937eb92f02e4952f6364afdff6e9605c3fba562b
jonathanvdc <jonathan.vdc@outlook.com>
2015-11-06 17:41:39 +0100

    Created a skeleton application, added libs

---
+++ src/muc/Program.fs
@@ -0,0 +1,9 @@
+﻿open Flame.Front.Cli
+
+[<EntryPoint>]
+let main argv = 
+    // TODO: register a micron project handler
+    // ProjectHandlers.RegisterHandler(MicronProjectHandler())
+    let compiler = ConsoleCompiler("muc", "the magnificent micron compiler", "https://github.com/jonathanvdc/micron/releases")
+    compiler.Compile(argv)
+    0
