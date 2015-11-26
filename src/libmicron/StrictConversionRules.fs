01051c673377a4712f577aa5dfa0c76d3fa210fc
jonathanvdc <jonathan.vdc@outlook.com>
2015-11-26 17:40:08 +0100

    Changed strict conversion rules... again

--- src/libmicron/StrictConversionRules.fs
+++ src/libmicron/StrictConversionRules.fs
@@ -6,17 +6,25 @@ open Flame.Compiler
 open Flame.Compiler.Expressions
 open Flame.Functional
 
+/// Defines a set of very strict conversion rules.
 type StrictConversionRules(typeNamer : IType -> string) =
     interface IConversionRules with
-        member this.HasImplicitConversion tyFrom tyTo = false
+        /// Implicit conversion between non-equivalent types are disallowed.
+        member this.HasImplicitConversion tyFrom tyTo = tyFrom.IsEquivalent(tyTo)
+
+        /// Tries to convert the given expression to the given type implicitly.
+        /// Implicit conversion between non-equivalent types are disallowed. 
         member this.ConvertImplicit expr tyTo = 
             let exprTy = expr.Type
             if exprTy.IsEquivalent(tyTo) then
                 expr
             else
+                // Make sure to insert an error node.
                 ExpressionBuilder.Error (LogEntry("Type mismatch", 
                                                   "Expected an expression of type '" + typeNamer tyTo + 
                                                   "'. Got one of type '" + typeNamer expr.Type + "'.")) 
                                         (ConversionExpression.Instance.Create(expr, tyTo))
+
+        /// Converts the given expression to the given type in an explicit manner.
         member this.ConvertExplicit expr tyTo = ConversionExpression.Instance.Create(expr, tyTo)
 
