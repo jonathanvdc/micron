a160012b89e44f031900c5b6fbe0cd7ed1ded7b1
jonathanvdc <jonathan.vdc@outlook.com>
2015-11-26 17:09:30 +0100

    Made strict conversion rules less strict

--- src/libmicron/StrictConversionRules.fs
+++ src/libmicron/StrictConversionRules.fs
@@ -10,9 +10,13 @@ type StrictConversionRules(typeNamer : IType -> string) =
     interface IConversionRules with
         member this.HasImplicitConversion tyFrom tyTo = false
         member this.ConvertImplicit expr tyTo = 
-            ExpressionBuilder.Error (LogEntry("Type mismatch", 
-                                              "Expected an expression of type '" + typeNamer tyTo + 
-                                              "'. Got one of type '" + typeNamer expr.Type + "'.")) 
-                                    (ConversionExpression.Instance.Create(expr, tyTo))
+            let exprTy = expr.Type
+            if exprTy.IsEquivalent(tyTo) then
+                expr
+            else
+                ExpressionBuilder.Error (LogEntry("Type mismatch", 
+                                                  "Expected an expression of type '" + typeNamer tyTo + 
+                                                  "'. Got one of type '" + typeNamer expr.Type + "'.")) 
+                                        (ConversionExpression.Instance.Create(expr, tyTo))
         member this.ConvertExplicit expr tyTo = ConversionExpression.Instance.Create(expr, tyTo)
 
