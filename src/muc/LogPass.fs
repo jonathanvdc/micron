0d3ec7169c0065e880a4f0666bd904998e68e8e7
jonathanvdc <jonathan.vdc@outlook.com>
2015-11-30 21:18:39 +0100

    Removed a silly semicolon

--- src/muc/LogPass.fs
+++ src/muc/LogPass.fs
@@ -8,5 +8,5 @@ type LogPass() =
     interface IPass<IStatement * IMethod * ICompilerLog, IStatement> with
         member this.Apply(tuple : IStatement * IMethod * ICompilerLog) : IStatement =
             let body, _, log = tuple
-            let logVisitor = new LoggingVisitor(log, true, false);
+            let logVisitor = new LoggingVisitor(log, true, false)
             logVisitor.Visit(body)
