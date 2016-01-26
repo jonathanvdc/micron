85fa772c86a8f3995f5f4f89d85a34b44f40fb55
Mauris Van Hauwe <maurisvh@users.noreply.github.com>
2016-01-26 18:08:01 +0100

    Implement a stdlib build script for Windows

---
+++ src/stdlib/build.bat
@@ -0,0 +1,5 @@
+dsc primlist.ds -platform ir -runtime clr -o bin/primlist.flo
+dsc primops.ds -platform ir -runtime clr -o bin/primops.flo
+dsc primio.ds -platform ir -runtime clr -o bin/primio.flo
+
+..\muc\bin\release\muc stdlib.mu bin/primops.flo bin/primio.flo bin/primlist.flo -platform ir -runtime clr -flower-lambda -o bin/stdlib.flo -repeat-command $@
